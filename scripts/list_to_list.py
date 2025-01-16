4fa9135c632612db4ac8c552a0a562f8529e9cd1
Fungustober <1theninjamaster@gmail.com>
2025-01-16 15:59:51 -0600

    JSON Makeover part 2
    Added symbol processing, italics processing, and bold processing to the export template. Started adding comments to the python scripts.

--- scripts/list_to_list.py
+++ scripts/list_to_list.py
@@ -2,16 +2,28 @@ import os
 import sys
 import re
 
-def convertList(setCode):
-	inputList = os.path.join('sets', setCode + '-files', setCode + '-raw.txt')
-	outputList = os.path.join('lists', setCode + '-list.txt')
-	with open(inputList, errors="ignore") as f:
-		raw = f.read()
-		cards_raw = raw.replace('\n','NEWLINE').replace('REPLACEME','\\n')
-	cards_raw = cards_raw.rstrip('\\n')
-	cards = cards_raw.split('\\n')
+#F = Fungustober's notes for understanding how all this works while she edits this to support JSON files for the main file
+#EDIT = Fungustober's marker for a part of code that needs edited to support JSON file
 
+def convertList(setCode):
+	#F: inputList = sets/SET-files/SET-raw.txt
+    inputList = os.path.join('sets', setCode + '-files', setCode + '-raw.txt') #EDIT - needs changed from .txt to .json
+	#F: outputList = lists/SET-list.txt
+    outputList = os.path.join('lists', setCode + '-list.txt')
+	#F: open up the inputList file
+    with open(inputList, errors="ignore") as f:
+		#F: now do the things
+        raw = f.read()
+		cards_raw = raw.replace('\n','NEWLINE').replace('REPLACEME','\\n') #EDIT - won't need this with the JSON file
+	cards_raw = cards_raw.rstrip('\\n') #EDIT - also won't need this with the JSON file
+	cards = cards_raw.split('\\n') #EDIT - will be able to replace this with a JSON crawler
+
+    #F: array of cards to skip
 	skipdex = []
+    #F: This basically gets any alt-arts in a single set and adds their index to a list of cards to skip.
+    #EDIT - we could probably do this a little better by going through the range of j,
+    #and checking if the name of cards[j] == the name of cards[i] + token not in cards[j].shape and Basic not in cards[j].type
+    #and instead of putting j in skipdex, we can put the card number of j in the skipdex, so we'd be able to iterate over the JSON cards array
 	for i in range(len(cards)):
 		tmpI = re.sub("\t[0-9]+\t.*", "", cards[i])
 		for j in range(i):
@@ -26,23 +38,31 @@ def convertList(setCode):
 	cards_land = []
 	cards_basic = []
 	cards_token = []
-
+    
+    #F: now go over the cards again
+    #EDIT - iterate over the JSON cards array
 	for i in range(len(cards)):
-		if i in skipdex:
+		#F: skip the card if it's in the skipdex
+        if i in skipdex:
 			continue
-		card = cards[i].split('\t')
+		card = cards[i].split('\t') #EDIT - won't need this with a JSON crawler
 		# name to include card number
+        #EDIT - replace the array indices with JSON keys; 0 = name; 4 = card number, 10 = shape
+        #EDIT - we're going to remake this a little, instead of storing the name in the main list, we'll be storing a new json object with
+            #name, number, and shape. I'll be figuring out how to do that once I get to working on updating this
 		card[0] = card[4] + ('t_' if 'token' in card[10] else '_') + card[0]
 		# card number to int
-		card[4] = int(card[4])
+		card[4] = int(card[4]) #EDIT - we won't need to make it an int, because it will already be one in the JSON file afaik
 		# filter sorting tags
-		notes = card[len(card) - 1]
+		notes = card[len(card) - 1] #EDIT - replace the final index check with the JSON key
 		if '!sort' in notes:
+            #F: notes = index of !sort + 6 to the end of the string
 			card[len(card) - 1] = notes[notes.index('!sort') + 6:]
 		else:
 			card[len(card) - 1] = 'zzz'
 
 		# clean color inputs
+        #EDIT - replace the array index with JSON keys; 1 = color
 		if card[1] == 'WR':
 			card[1] = 'RW'
 		if card[1] == 'WG':
@@ -51,6 +71,7 @@ def convertList(setCode):
 			card[1] = 'GU'
 
 		# clean color identity inputs
+        #EDIT - replace the array index with JSON keys; 5 = color_identity
 		if card[5] == 'WR':
 			card[5] = 'RW'
 		if card[5] == 'WG':
@@ -59,6 +80,7 @@ def convertList(setCode):
 			card[5] = 'GU'
 
 		# clean rarities
+        #EDIT - replace the array index with JSON keys; 2 = rarity
 		if card[2] == 'common':
 			card[2] = 4
 		elif card[2] == 'uncommon':
@@ -71,6 +93,7 @@ def convertList(setCode):
 			card[2] = 5
 
 		# sort types
+        #EDIT - replace the array index with JSON keys; 10 = shape, 1 = color, 3 = type
 		if 'token' in card[10]:
 			cards_token.append(card)
 		elif len(card[1]) > 1:
@@ -92,6 +115,7 @@ def convertList(setCode):
 	cards_r = []
 	cards_g = []
 	
+    #EDIT - replace the array index with JSON keys
 	for card in cards_mono:
 		if card[1] == 'W':
 			cards_w.append(card)
@@ -104,19 +128,25 @@ def convertList(setCode):
 		if card[1] == 'G':
 			cards_g.append(card)
 
-	row_count = max(len(cards_w),len(cards_u),len(cards_b),len(cards_r),len(cards_g))
-	cards_mono_arr = [cards_w, cards_u, cards_b, cards_r, cards_g]
+    row_count = max(len(cards_w),len(cards_u),len(cards_b),len(cards_r),len(cards_g))
+    #F: put all the monocolor things into a big array
+    cards_mono_arr = [cards_w, cards_u, cards_b, cards_r, cards_g]
 	
 	for x in range(len(cards_mono_arr)):
 		card_arr = cards_mono_arr[x]
+        #F: x[2] = rarity, x[len(x) - 1] = notes, x[4] = card number
+        #EDIT - replace them with the appropriate JSON keys
 		cards_mono_arr[x] = sorted(card_arr, key=lambda x : (x[2], x[len(x) - 1], x[4]))
 	
+    #F: 'e' and 'er' are references to blank pngs for spoilers
 	for row in range(row_count):
 		for card_arr in cards_mono_arr:
 			if (row >= len(card_arr)):
 				master_list.append('e')
 			else:
-				master_list.append(card_arr[row][0])
+				#F: card_arr[row][0] = name
+                #EDIT - replace it with the appropriate JSON key
+                master_list.append(card_arr[row][0])
 
 	for x in range(5):
 		master_list.append('er')
@@ -134,6 +164,7 @@ def convertList(setCode):
 	cards_rw = []
 	cards_gold = []
 
+    #EDIT - replace the index with a JSON key
 	for card in cards_multi:
 		if card[1] == 'WU':
 			cards_wu.append(card)
@@ -157,20 +188,25 @@ def convertList(setCode):
 			cards_rw.append(card)
 		else:
 			cards_gold.append(card)
-	
+    
 	# ally pairs
 	row_count = max(len(cards_wu),len(cards_ub),len(cards_br),len(cards_rg),len(cards_gw))
 	cards_ally_arr = [cards_wu, cards_ub, cards_br, cards_rg, cards_gw]
 
 	for x in range(len(cards_ally_arr)):
 		card_arr = cards_ally_arr[x]
+        #F: x[2] = rarity, x[len(x) - 1] = notes, x[4] = card number
+        #EDIT - replace them with the appropriate JSON keys
 		cards_ally_arr[x] = sorted(card_arr, key=lambda x : (x[2], x[len(x) - 1], x[4]))
 	
+    #F: 'e' and 'er' are references to blank pngs for spoilers
 	for row in range(row_count):
 		for card_arr in cards_ally_arr:
 			if (row >= len(card_arr)):
 				master_list.append('e')
 			else:
+				#F: card_arr[row][0] = name
+                #EDIT - replace it with the appropriate JSON key
 				master_list.append(card_arr[row][0])
 
 	for x in range(5):
@@ -182,6 +218,8 @@ def convertList(setCode):
 
 	for x in range(len(cards_enemy_arr)):
 		card_arr = cards_enemy_arr[x]
+        #F: x[2] = rarity, x[len(x) - 1] = notes, x[4] = card number
+        #EDIT - replace them with the appropriate JSON keys
 		cards_enemy_arr[x] = sorted(card_arr, key=lambda x : (x[2], x[len(x) - 1], x[4]))
 	
 	for row in range(row_count):
@@ -189,15 +227,21 @@ def convertList(setCode):
 			if (row >= len(card_arr)):
 				master_list.append('e')
 			else:
+				#F: card_arr[row][0] = name
+                #EDIT - replace it with the appropriate JSON key
 				master_list.append(card_arr[row][0])
 
 	for x in range(5):
 		master_list.append('er')
 
 	# 3c+ cards
+    #F: x[1] = color, x[2] = rarity, x[len(x) - 1] = notes, x[4] = card number
+    #EDIT - replace them with the appropriate JSON keys
 	cards_gold = sorted(cards_gold, key=lambda x : (len(x[1]), x[2], x[len(x) - 1], x[4]))
-
+    
 	for card in cards_gold:
+        #F: card_arr[row][0] = name
+        #EDIT - replace it with the appropriate JSON key
 		master_list.append(card[0])
 
 	if len(cards_gold) % 5 != 0:
@@ -212,7 +256,9 @@ def convertList(setCode):
 	cards_brown = sorted(cards_brown, key=lambda x : (x[2], x[len(x) - 1], x[4]))
 
 	for card in cards_brown:
-		master_list.append(card[0])
+		#F: card_arr[row][0] = name
+        #EDIT - replace it with the appropriate JSON key
+        master_list.append(card[0])
 
 	if len(cards_brown) % 5 != 0:
 		for x in range(5 - (len(cards_brown) % 5)):
@@ -240,6 +286,8 @@ def convertList(setCode):
 	lands_rw = []
 	lands_other = []
 
+    #F: 5 = Color Identity
+    #EDIT - replace them with the proper JSON key
 	for card in cards_land:
 		if card[5] == 'W':
 			lands_w.append(card)
@@ -279,6 +327,8 @@ def convertList(setCode):
 	lands_mono_arr = [lands_w, lands_u, lands_b, lands_r, lands_g]
 
 	for x in range(len(lands_mono_arr)):
+        #F: x[2] = rarity, x[len(x) - 1] = notes, x[4] = card number
+        #EDIT - replace them with the appropriate JSON keys
 		card_arr = lands_mono_arr[x]
 		lands_mono_arr[x] = sorted(card_arr, key=lambda x : (x[2], x[len(x) - 1], x[4]))
 	
@@ -287,6 +337,8 @@ def convertList(setCode):
 			if (row >= len(card_arr)):
 				master_list.append('e')
 			else:
+				#F: card_arr[row][0] = name
+                #EDIT - replace it with the appropriate JSON key
 				master_list.append(card_arr[row][0])
 
 	# ally pairs
@@ -294,6 +346,8 @@ def convertList(setCode):
 	lands_ally_arr = [lands_wu, lands_ub, lands_br, lands_rg, lands_gw]
 
 	for x in range(len(lands_ally_arr)):
+        #F: x[2] = rarity, x[len(x) - 1] = notes, x[4] = card number
+        #EDIT - replace them with the appropriate JSON keys
 		card_arr = lands_ally_arr[x]
 		lands_ally_arr[x] = sorted(card_arr, key=lambda x : (x[2], x[len(x) - 1], x[4]))
 	
@@ -302,6 +356,8 @@ def convertList(setCode):
 			if (row >= len(card_arr)):
 				master_list.append('e')
 			else:
+				#F: card_arr[row][0] = name
+                #EDIT - replace it with the appropriate JSON key
 				master_list.append(card_arr[row][0])
 
 	# enemy pairs
@@ -309,6 +365,8 @@ def convertList(setCode):
 	lands_enemy_arr = [lands_wb, lands_bg, lands_gu, lands_ur, lands_rw]
 
 	for x in range(len(lands_enemy_arr)):
+        #F: x[2] = rarity, x[len(x) - 1] = notes, x[4] = card number
+        #EDIT - replace them with the appropriate JSON keys
 		card_arr = lands_enemy_arr[x]
 		lands_enemy_arr[x] = sorted(card_arr, key=lambda x : (x[2], x[len(x) - 1], x[4]))
 	
@@ -317,6 +375,8 @@ def convertList(setCode):
 			if (row >= len(card_arr)):
 				master_list.append('e')
 			else:
+				#F: card_arr[row][0] = name
+                #EDIT - replace it with the appropriate JSON key
 				master_list.append(card_arr[row][0])
 
 	if len(cards_land) - len(lands_other) > 0:
@@ -324,9 +384,13 @@ def convertList(setCode):
 			master_list.append('er')
 
 	# other lands
+    #F: x[2] = rarity, x[len(x) - 1] = notes, x[4] = card number
+    #EDIT - replace them with the appropriate JSON keys
 	lands_other = sorted(lands_other, key=lambda x : (x[2], x[len(x) - 1], x[4]))
 
 	for card in lands_other:
+        #F: 0 = name
+        #EDIT - replace it with the appropriate JSON key
 		master_list.append(card[0])
 
 	if len(lands_other) % 5 != 0:
@@ -338,11 +402,16 @@ def convertList(setCode):
 			master_list.append('er')
 
 	# basic lands
+    #F: x[2] = rarity, x[len(x) - 1] = notes, x[4] = card number
+    #EDIT - replace them with the appropriate JSON keys
 	cards_basic = sorted(cards_basic, key=lambda x : (x[2], x[len(x) - 1], x[4]))
 
 	# if you're reading this, leave me alone, I did this in like 2 minutes I know it's bad code
+    #F: well, I probably wouldn't be able to do much better to be completely fair
 	basic_types = []
 	for card in cards_basic:
+        #F: 3 = type
+        #EDIT - you know the deal
 		if card[3] not in basic_types:
 			basic_types.append(card[3])
 
@@ -353,6 +422,8 @@ def convertList(setCode):
 	for n in range(land_count): 
 		for x in range(len(cards_basic)):
 			if (x % land_count == n):
+                #F: 0 = name
+                #EDIT - you know the deal
 				master_list.append(cards_basic[x][0])
 
 	if len(cards_basic) % 5 != 0:
@@ -364,15 +435,21 @@ def convertList(setCode):
 			master_list.append('er')
 
 	# tokens
+    #F: x[2] = rarity, x[len(x) - 1] = notes, x[4] = card number
+    #EDIT - replace them with the appropriate JSON keys
 	cards_token = sorted(cards_token, key=lambda x : (x[2], x[len(x) - 1], x[4]))
 
 	for card in cards_token:
+        #F: 0 = name
+        #EDIT - you know the deal
 		master_list.append(card[0])
 
 	if len(cards_token) % 5 != 0:
 		for x in range(5 - (len(cards_token) % 5)):
 			master_list.append('e')
 
+    #F: lists/SET-list.txt finally comes into play
+    #F: \ufeff is the Byte Order Mark character that, according to Egg, was sneaking into card names and had to be forcibly removed
 	with open(outputList, 'w') as f:
 		for card_name in master_list:
 			print(card_name.replace(u'\ufeff', ''), file=f)
@@ -383,7 +460,7 @@ def convertList(setCode):
 
 
 
-
+#F: why are there so many blank lines down here
 
 
 
--- scripts/print_html_for_set.py
+++ scripts/print_html_for_set.py
@@ -1,9 +1,14 @@
 import os
 import sys
 
-def generateHTML(code):
-	output_html_file = "sets/" + code + ".html"
+#F = Fungustober's notes for understanding how all this works while she edits this to support JSON files for the main file
+#EDIT = Fungustober's marker for a part of code that needs edited to support JSON file
 
+def generateHTML(code):
+    #F: /sets/SET.html
+	output_html_file = "sets/" + code + ".html"
+    
+    #F: sets/SET-files/SET-fullname.txt
 	with open(os.path.join("sets", code + "-files", code + "-fullname.txt"), encoding='utf-8-sig') as f:
 		set_name = f.read()
 
@@ -208,7 +213,9 @@ def generateHTML(code):
 </style>
 <body>
 	'''
-
+    
+    #F: /resources/snippets/header.txt
+    #F: we've seen this one in print_html_for_spoiler.py already. doesn't need any modifications
 	with open(os.path.join('resources', 'snippets', 'header.txt'), encoding='utf-8-sig') as f:
 		snippet = f.read()
 		html_content += snippet
@@ -220,6 +227,7 @@ def generateHTML(code):
 				<img class="set-logo" src="/sets/''' + code + '''-files/icon.png">
 				<div class="set-title">''' + set_name + '''</div>'''
 
+    #F: sets/SET-files/SET-draft.txt
 	if os.path.exists(os.path.join('sets', code + '-files', code + '-draft.txt')):
 		html_content += '''<a href="/sets/''' + code + '''-files/''' + code + '''-draft.txt" download>Draft me!</a>
 		'''
@@ -249,6 +257,9 @@ def generateHTML(code):
 		snippet = f.read()
 		html_content += snippet
 
+    #F: this uses card_list_arrayified from load-files.txt
+    #EDIT - rework the following section where it uses card_list_arrayified and make it use the JSON structure instead
+    #F: 11 = code, 10 = shape, 3 = type
 	html_content += '''
 
 			for (let i = 0; i < card_list_arrayified.length; i++)
@@ -322,6 +333,9 @@ def generateHTML(code):
 
 		'''
 
+    #F: this is where the previously mentioned compareFunction is from
+    #F: it looks at the shape (10), set code (11), number (4), name (0), cost (6), color (1), and rarity (2) of a card
+    #F: we'll need to do some work to make that compatible with JSON
 	with open(os.path.join('resources', 'snippets', 'compare-function.txt'), encoding='utf-8-sig') as f:
 		snippet = f.read()
 		html_content += snippet
@@ -330,10 +344,16 @@ def generateHTML(code):
 
 		'''
 
+    #F: /resources/snippets/tokenize-symbolize.txt
+    #F: this holds the isDecimal function used in compare-function.txt, as well as something for encoding/decoding symbols
+    #F: the encoding/decoding text symbols one doesn't seem to work here.
+    #F: we probably won't have to use the tokenizing/symbolizing part of tokenize-symbolize if we add the symbol processor into the export-template
+    #F: which should make things a lot easier.
 	with open(os.path.join('resources', 'snippets', 'tokenize-symbolize.txt'), encoding='utf-8-sig') as f:
 		snippet = f.read()
 		html_content += snippet
 
+    #EDIT - modify this to use the JSON structure; 0 = name
 	html_content += '''
 
 		function gridifyCard(card_stats) {
@@ -346,6 +366,25 @@ def generateHTML(code):
 
 		'''
 
+    #F: /resources/snippets/img-container-defs.txt
+    #F: this has more stuff that we need to edit
+    #EDIT - make this use the JSON structure; 0 = name, 6 = color, 3 = type, 7 = , 8 = , 12 = , 10 = shape, 13 = , 17 = , 15 = , 18 = , 
+    #20 = , 19 = , 21 = , 11 = 
+    #F: ah, this is also where symbolize from tokenize-symbolize is used
+    #F: let pattern1 = /([0-9X]*[WUBRGCT/]+)([ :,\.<]|$)/g;
+	#F: let pattern2 = /(?<![a-z] |\/[0-9X]*)([0-9X]+)([:,]| <i>\()/g;
+	#F: let pattern3 = /([Pp]ay[s]* |[Cc]ost[s]* |[Ww]ard )([0-9X])(?! life)/g;
+	#F: let pattern4 = /(Equip [^(<]*)([0-9XWUBRGC/]+)/g;
+    #F: let regexHTML = HTML.replace(pattern1, function (match, group1, group2) { return symbolize(group1) + group2; });
+	#F: regexHTML = regexHTML.replace(pattern2, function (match, group1, group2) { return symbolize(group1) + group2; });
+    #F: regexHTML = regexHTML.replace(pattern3, function (match, group1, group2) { return group1 + symbolize(group2); });
+	#F: regexHTML = regexHTML.replace(pattern4, function (match, group1, group2) { return group1 + symbolize(group2); });
+    #F: yeah, this would be simplified a whole ton if I used the symbol processor I made in the exporter
+    #F: we wouldn't have to do most of this hell, I think we could probably just put the 
+    #F: '<span class="mana mana-cost mana-' and '"></span>' from tokenize-symbolize into the card text during the export process
+    #F: but that wouldn't play well with things like Lackeybot. I think we'll still have to deal with that in img-container-defs
+    #F: however, we can deal with it differently. Instead of looking for all the possible things in text, we can just look for {}
+    #F: in bits of text within the JSON card object.
 	with open(os.path.join('resources', 'snippets', 'img-container-defs.txt'), encoding='utf-8-sig') as f:
 		snippet = f.read()
 		html_content += snippet
@@ -365,6 +404,9 @@ def generateHTML(code):
 
 		'''
 
+    #F: resources/snippets/random-card.txt
+    #F: we've already looked at this in print_html_for_spoiler
+    #F: I think that's everything
 	with open(os.path.join('resources', 'snippets', 'random-card.txt'), encoding='utf-8-sig') as f:
 		snippet = f.read()
 		html_content += snippet
